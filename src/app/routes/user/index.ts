982995cbeb76915031bbca815d9e6caca712095a
Aashish-Dahal <ashishdahal490@gmail.com>
2023-03-01 16:58:03 +0545

    chore: new routes

--- src/app/routes/user/index.ts
+++ src/app/routes/user/index.ts
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { register } from "../../controllers/firebase_user";
-import { getUser } from "../../controllers/user";
+import {
+  createUser,
+  getAllUser,
+  getOneUser,
+  updateUser,
+} from "../../controllers/user";
 
 const router = Router();
 
 // GET user/docs
 
-router.use("/user", getUser);
-router.use("/register", register);
+router.get("/user", getAllUser);
+router.get("/user/:id", getOneUser);
+router.post("/user/create", createUser);
+router.put("/user/update/:id", updateUser);
+
+router.post("/register", register);
 
 export default router;
