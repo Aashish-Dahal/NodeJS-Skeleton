4c851e5d90441e7a5c260f0c4fe2b21ecc2979d6
Aashish-Dahal <ashishdahal490@gmail.com>
2023-03-01 16:56:57 +0545

    feat: mysql crud

--- src/app/controllers/user/index.ts
+++ src/app/controllers/user/index.ts
@@ -1,11 +1,55 @@
 import { NextFunction, Request, Response } from "express";
+import { pool } from "../../../config/mysql";
 import { asyncHandler } from "../../middleware/async";
 
-export const getUser = asyncHandler(
+export const getAllUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({
+    const user = await pool.query("SELECT * FROM user");
+    res.status(201).json({
       success: true,
-      user: "user data",
+      user: user[0],
+    });
+  }
+);
+export const getOneUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await pool.query(`SELECT * FROM user WHERE id = ? `, [
+      req.params.id,
+    ]);
+    res.status(201).json({
+      success: true,
+      user: user[0][0],
+    });
+  }
+);
+export const createUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await pool.query(`INSERT INTO user SET ?`, req.body);
+    res.status(201).json({
+      success: true,
+      message: "user created successfully",
+    });
+  }
+);
+export const updateUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name } = req.body;
+    await pool.query(`UPDATE user SET name = ? WHERE id = ?`, [
+      name,
+      req.params.id,
+    ]);
+    res.status(201).json({
+      success: true,
+      message: `User with the record id ${req.params.id} has been updated`,
+    });
+  }
+);
+export const deleteUser = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction) => {
+    await pool.query(`DELETE * FROM user WHERE id = ? `, [req.params.id]);
+    res.status(201).json({
+      success: true,
+      user: `User with the record id ${req.params.id} has been removed`,
     });
   }
 );
